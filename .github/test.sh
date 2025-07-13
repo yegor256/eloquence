a8e0829cf64fcc0a702f68c4e6b7bdb72495a0c2
Yegor Bugayenko <yegor256@gmail.com>
2025-07-13 12:01:02 +0300

    #1: Refactor build process to ensure proper cleaning and compilation.

--- .github/test.sh
+++ .github/test.sh
@@ -2,11 +2,11 @@
 # SPDX-FileCopyrightText: Copyright (c) Yegor Bugayenko, 2025
 # SPDX-License-Identifier: MIT
 
-set -e -o pipefail
+set -ex -o pipefail
 
 tmp=.test
 mkdir -p "${tmp}/eloquence"
-cp -R ./*.sh ltex.json eloquence.defs vale.ini vale-styles "${tmp}/eloquence"
+cp -R eloquence.defs ./*.sh ltex.json vale.ini vale-styles "${tmp}/eloquence"
 cd "${tmp}"
 
 mkdir -p tex
@@ -14,10 +14,11 @@ echo "Hello, world." > tex/hello.tex
 
 echo "include eloquence/eloquence.defs" > Makefile
 
-make eloquence
-make texsc
-make .eloquence/fake/hello.tex
+make clean
+make .eloquence/double/hello.tex
 make .eloquence/pdf/hello.pdf
 make .eloquence/md/hello.md
+make texsc
 make vale
 make ltex
+make eloquence
