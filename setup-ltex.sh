29f477a227361360366ad4b5fee67ac8236cdb63
Yegor Bugayenko <yegor256@gmail.com>
2025-07-20 15:44:29 +0300

    Refactor setup script to check for ltex-cli existence.

--- setup-ltex.sh
+++ setup-ltex.sh
@@ -8,7 +8,7 @@ target=$1
 
 version=16.0.0
 tgz=${target}/ltex.tgz
-if [ ! -e "${tgz}" ] || [ ! -e "${target}/ltex" ]; then
+if [ ! -e "${tgz}" ] || [ ! -x "${target}/ltex/bin/ltex-cli" ]; then
     wget -N "https://github.com/valentjn/ltex-ls/releases/download/${version}/ltex-ls-${version}-mac-x64.tar.gz" -qq -O "${tgz}"
     rm -rf "${target}/ltex-ls-${version}"
     tar -xzf "${tgz}" -C "${target}"
